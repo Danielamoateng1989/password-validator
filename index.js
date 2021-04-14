a55fdc0cf4148296ace1166e812a7d7d6bd9334f
Daniel Amoateng <danielamoateng@iMac-2.local>
2021-04-14 05:04:58 -0400

    Set password length value to equal 8

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ function validatePassword(password) {
   const specialChars = '!@#$%'
 
 
-  // for (let i = 0; i < password.length; i++) {
-  //   if (passwordLenth >= 8) return true
+  for (let i = 0; i < password.length; i++) {
+    if (passwordLenth >= 8) return true
 
-  //   if (numerics.password) return true
+    //if (numerics.password) return true
 
-  //   if (lowerCase.password) return true
+    //if (lowerCase.password) return true
 
-  //   if (upperCase.password) return true
+    //if (upperCase.password) return true
 
-  //   if (specialChars.password) return true
-  // }
+    //if (specialChars.password) return true
+  }
 
 
 
