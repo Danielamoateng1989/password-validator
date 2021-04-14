aad33ff51e763912490151092534628a10e93ebc
Daniel Amoateng <danielamoateng@iMac-2.local>
2021-04-14 05:02:37 -0400

    Declares constants with variables and intialize it

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// Write a function called validatePassword 
+// Function takes password as a parameter
+// Must be a minimum of 8 characters
+// Must contain at least one lower case letter
+// Must contain at least one upper case letter
+// Must contain at least one numeric value
+// Must contain at least one special character
+
+
 function validatePassword(password) {
   const passwordLenth = 8
   const numerics = '0123456789'
@@ -6,24 +15,25 @@ function validatePassword(password) {
   const specialChars = '!@#$%'
 
 
-  for (let i = 0; i < password.length; i++) {
-    if (passwordLenth >= 8) return true
+  // for (let i = 0; i < password.length; i++) {
+  //   if (passwordLenth >= 8) return true
 
-    if (numerics.password) return true
+  //   if (numerics.password) return true
 
-    if (lowerCase.password) return true
+  //   if (lowerCase.password) return true
 
-    if (upperCase.password) return true
+  //   if (upperCase.password) return true
 
-    if (specialChars.password) return true
-  }
+  //   if (specialChars.password) return true
+  // }
 
 
 
-  let validatePassword = (password) => {
-    // eslint-disable-next-line max-len
-    if ((passwordLenth(password) && (numerics(password) && (lowerCase(password) && (upperCase(password) && (specialChars(password))))))) return true
-  }
+  // eslint-disable-next-line no-unused-vars
+  // let validatePassword = (password) => {
+  //   // eslint-disable-next-line max-len
+  //   if ((passwordLenth(password) && (numerics(password) && (lowerCase(password) && (upperCase(password) && (specialChars(password))))))) return true
+  // }
 }
 
 
